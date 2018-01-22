7b1baef7fe0de68c86480d9e0ce9b5edbe77ded0
MatTheGreat <c00190038@itcarlow.ie>
2018-01-22 12:52:54 +0000

    ProducerConsumer Started

---
+++ ProducerConsumer/ProducerConsumer/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+
+/*
+int buf, p = 0, c = 0;
+process Producer
+{
+	int a[n];
+	while (p < n)
+	{
+		<await(p == c);>
+		buf = a[p];
+		p = p + 1;
+	}
+}
+
+process Consumer
+{
+	int b[n];
+	while (c < n)
+	{
+		< await(p < c); >
+		b[c] = buf;
+		c = c + 1;
+	}
+}
+*/
+
+void CheckFileForPattern()
+{
+
+}
+
+int main()
+{
+
+	/*
+	std::string line1, line2;
+	read a line of input from stdin into line1;
+	while(line1 != EOF)
+	{
+		co look for pattern in line1;
+		if(pattern in line1)
+		{
+			write1;
+		}
+		//read next line of input into line2
+		oc;
+			line1 = line2;
+	}
+	*/
+
+}
+
